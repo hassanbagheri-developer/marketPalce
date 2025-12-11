28ae0bea34b124d2a687e45f3ac48e60badbe990
H.Bagheri <hassanbagheri233@gmail.com>
2025-12-11 11:50:43 +0330

    init commit

---
+++ src/main/java/com/example/marketPlace/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.marketPlace;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MarketPlaceApplication.class);
+	}
+
+}
