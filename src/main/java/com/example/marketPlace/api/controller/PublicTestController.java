b50057ae92d9b87c4f079a291aa467e647d15246
H.Bagheri <hassanbagheri233@gmail.com>
2025-12-27 23:07:32 +0330

    update

--- src/main/java/com/example/marketPlace/api/controller/PublicTestController.java
+++ src/main/java/com/example/marketPlace/api/controller/PublicTestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PublicTestController {
 
 
-    @Value("${my.value.myName}")
+    @Value("${my.value.myName:aaa}")
     private String myName;
 
     @GetMapping("/test1")
